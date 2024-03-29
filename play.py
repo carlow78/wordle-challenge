8729fc58f52b1986a2ac97df5325208f7cd0e00a
John Lynch <johnf.lynch@yahoo.ie>
2024-03-29 19:31:58 +0000

    game function
    function guess correct
    attempts left and remaining

--- play.py
+++ play.py
@@ -1,5 +1,14 @@
+from run import Wordle
+
 def main():
-    print("Hello world")
+    wordle = Wordle("TAXES")
+    
+    while wordle.guess_attempt:
+       x = input("Enter your guess:\n")
+       wordle.attempt(x)
+        
+    if wordle.game_over:
+        print("You have guessed the word. Congrats")
 
 if __name__ == "__main__":
     main()
--- run.py
+++ run.py
@@ -1,3 +1,33 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+class Wordle:
+
+    MAX_GUESSES = 6
+    WORD_LENGTH = 5
+
+    def __init__(self, secret: str):
+        self.secret: str = secret
+        self.guesses = []
+        pass
+
+    def attempt(self, word: str):
+        self.guesses.append(word)
+
+    # Function when the word has been guessed correctly. Game over.
+
+    @property # Allows us to call the function without ()
+    def game_over(self):
+        return len(self.guesses) > 0 and self.guesses[-1] == self.secret
+    
+    # Function for remaining attempts
+    @property
+    def remain_attempts(self) -> int:
+       return self.MAX_GUESSES - len(self.guesses)
+
+    
+    # Function to allow player only 5 guesses
+    
+    @property 
+    def guess_attempt(self):
+        return self.remain_attempts > 0 and not self.game_over
+    
+
