de3ffa03eba136333eaf7edc292c49cf8e6bf950
John Lynch <johnf.lynch@yahoo.ie>
2024-03-30 15:47:33 +0000

    Update play.py
    add if statement that checks if guess should be exactly 5 letters

--- play.py
+++ play.py
@@ -5,9 +5,14 @@ def main():
     
     while wordle.guess_attempt:
        i = input("Enter your guess:")
+
+       if len(i) != wordle.WORD_LENGTH:
+           print(f"Guess must be {wordle.WORD_LENGTH} characters long")
+           continue
+    
        wordle.attempt(i)
        result = wordle.guess(i)
-       print(*result, sep="\n")
+       print(*result, sep="\n")                 
 
     if wordle.game_over:
         print("You have guessed the word. Congrats")
